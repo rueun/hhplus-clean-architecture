43db2e091492db3495761c497085431d652db06d
rueun <dkfah1229@naver.com>
2024-10-02 23:10:46 +0900

    test: 테스트 코드 리팩토링

--- src/test/java/io/hhplus/cleanarchitecture/lecture/application/service/LectureServiceTest.java
+++ src/test/java/io/hhplus/cleanarchitecture/lecture/application/service/LectureServiceTest.java
@@ -1,9 +1,12 @@
 package io.hhplus.cleanarchitecture.lecture.application.service;
 
 import io.hhplus.cleanarchitecture.common.time.TimeProvider;
-import io.hhplus.cleanarchitecture.lecture.application.dto.EnrollLectureCommand;
+import io.hhplus.cleanarchitecture.lecture.application.dto.LectureEnrollmentInfo;
+import io.hhplus.cleanarchitecture.lecture.application.dto.command.EnrollLectureCommand;
+import io.hhplus.cleanarchitecture.lecture.application.dto.LectureWithItems;
 import io.hhplus.cleanarchitecture.lecture.domain.entity.Lecture;
 import io.hhplus.cleanarchitecture.lecture.domain.entity.LectureEnrollment;
+import io.hhplus.cleanarchitecture.lecture.domain.entity.LectureItem;
 import io.hhplus.cleanarchitecture.lecture.domain.repository.LectureEnrollmentRepository;
 import io.hhplus.cleanarchitecture.lecture.domain.repository.LectureRepository;
 import org.junit.jupiter.api.Test;
@@ -13,8 +16,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
+import static org.assertj.core.api.BDDAssertions.tuple;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
@@ -41,15 +50,16 @@ class LectureServiceTest {
         // Given
         EnrollLectureCommand command = EnrollLectureCommand.builder()
                 .lectureId(1L)
+                .lectureItemId(1L)
                 .userId(1L)
                 .build();
 
-        given(lectureRepository.getById(1L)).willReturn(
-                Lecture.builder()
+        given(lectureRepository.getItemById(1L, 1L)).willReturn(
+                LectureItem.builder()
                         .id(1L)
-                        .maxEnrollmentCount(30)
-                        .enrolledCount(29)
-                        .enrollOpenAt(LocalDateTime.parse("2024-10-01T10:00"))
+                        .capacity(30)
+                        .remainingCapacity(30)
+                        .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
                         .build());
 
         given(timeProvider.now()).willReturn(LocalDateTime.parse("2024-10-01T10:00"));
@@ -58,9 +68,11 @@ class LectureServiceTest {
         lectureService.enroll(command);
 
         // Then
-        verify(lectureRepository).getById(1L);
-        verify(lectureRepository).save(any(Lecture.class));
-        verify(lectureEnrollmentRepository).save(any(LectureEnrollment.class));
+        assertAll(
+                () -> verify(lectureRepository).getItemById(1L, 1L),
+                () -> verify(lectureRepository).save(any(LectureItem.class)),
+                () -> verify(lectureEnrollmentRepository).save(any(LectureEnrollment.class))
+        );
     }
 
     @Test
@@ -68,15 +80,16 @@ class LectureServiceTest {
         // Given
         EnrollLectureCommand command = EnrollLectureCommand.builder()
                 .lectureId(1L)
+                .lectureItemId(1L)
                 .userId(1L)
                 .build();
 
-        given(lectureRepository.getById(1L)).willReturn(
-                Lecture.builder()
+        given(lectureRepository.getItemById(1L, 1L)).willReturn(
+                LectureItem.builder()
                         .id(1L)
-                        .maxEnrollmentCount(30)
-                        .enrolledCount(29)
-                        .enrollOpenAt(LocalDateTime.parse("2024-10-01T10:00"))
+                        .capacity(30)
+                        .remainingCapacity(29)
+                        .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
                         .build());
 
         given(timeProvider.now()).willReturn(LocalDateTime.parse("2024-10-01T10:00"));
@@ -90,28 +103,30 @@ class LectureServiceTest {
                 .hasMessage("해당 유저는 이미 수강신청을 했습니다.");
     }
 
+
     @Test
-    void 수강신청_기간이_아닌_경우_수강신청을_할_수_없다() {
+    void 수강신청_시간이_지난_경우_수강신청을_할_수_없다() {
         // Given
         EnrollLectureCommand command = EnrollLectureCommand.builder()
                 .lectureId(1L)
+                .lectureItemId(1L)
                 .userId(1L)
                 .build();
 
-        given(lectureRepository.getById(1L)).willReturn(
-                Lecture.builder()
+        given(lectureRepository.getItemById(1L, 1L)).willReturn(
+                LectureItem.builder()
                         .id(1L)
-                        .maxEnrollmentCount(30)
-                        .enrolledCount(29)
-                        .enrollOpenAt(LocalDateTime.parse("2024-10-01T10:00"))
+                        .capacity(30)
+                        .remainingCapacity(29)
+                        .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
                         .build());
 
-        given(timeProvider.now()).willReturn(LocalDateTime.parse("2024-10-01T09:59"));
+        given(timeProvider.now()).willReturn(LocalDateTime.parse("2024-10-01T10:01"));
 
         // When & Then
         thenThrownBy(() -> lectureService.enroll(command))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessage("수강 신청 기간이 아닙니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("수강 신청 시간이 지났습니다.");
     }
 
     @Test
@@ -119,22 +134,116 @@ class LectureServiceTest {
         // Given
         EnrollLectureCommand command = EnrollLectureCommand.builder()
                 .lectureId(1L)
+                .lectureItemId(1L)
                 .userId(1L)
                 .build();
 
-        given(lectureRepository.getById(1L)).willReturn(
-                Lecture.builder()
+        given(lectureRepository.getItemById(1L, 1L)).willReturn(
+                LectureItem.builder()
                         .id(1L)
-                        .maxEnrollmentCount(30)
-                        .enrolledCount(30)
-                        .enrollOpenAt(LocalDateTime.parse("2024-10-01T10:00"))
+                        .capacity(30)
+                        .remainingCapacity(0)
+                        .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
                         .build());
-
-        given(timeProvider.now()).willReturn(LocalDateTime.parse("2024-10-01T10:00"));
 
         // When & Then
         thenThrownBy(() -> lectureService.enroll(command))
-                .isInstanceOf(IllegalStateException.class)
-                .hasMessage("최대 수강 인원을 초과했습니다.");
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("잔여 수량이 없습니다.");
+    }
+
+    @Test
+    void 사용자는_신청_가능한_강의_목록을_가져올_수_있다() {
+        // Given
+        Lecture lecture = Lecture.builder()
+                .title("특강1")
+                .id(1L)
+                .build();
+
+        LectureItem lectureItem1 = LectureItem.builder()
+                .lectureId(1L)
+                .capacity(30)
+                .remainingCapacity(20)
+                .build();
+
+        LectureItem lectureItem2 = LectureItem.builder()
+                .lectureId(1L)
+                .capacity(30)
+                .remainingCapacity(0)
+                .build();
+
+        given(lectureRepository.getAvailableLectures(1L)).willReturn(List.of(lecture));
+        given(lectureRepository.getLectureItemMap(List.of(1L))).willReturn(Map.of(
+                1L, List.of(lectureItem1, lectureItem2)
+        ));
+
+        // When
+        List<LectureWithItems> availableLectures = lectureService.getAvailableLectures(1L);
+
+        // Then
+        assertAll(
+                () -> assertEquals(1, availableLectures.size()),
+                () -> assertEquals(1, availableLectures.get(0).getItems().size()), // 잔여 수량이 있는 강의 아이템만 포함되어야 함
+                () -> assertThat(availableLectures.get(0).getItems()).extracting("lectureId", "capacity", "remainingCapacity")
+                        .containsExactly(
+                                tuple(1L, 30, 20)
+                        )
+        );
+    }
+
+    @Test
+    void 사용자는_신청한_모든_강의_목록을_가져올_수_있다() {
+        // Given
+        Long userId = 1L;
+
+        Lecture lecture1 = Lecture.builder().id(1L).title("특강1").build();
+        Lecture lecture2 = Lecture.builder().id(2L).title("특강2").build();
+
+        LectureItem lectureItem1 = LectureItem.builder()
+                .id(1L)
+                .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
+                .build();
+        LectureItem lectureItem2 = LectureItem.builder()
+                .id(2L)
+                .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
+                .build();
+
+        LectureEnrollment enrollment1 = LectureEnrollment.builder()
+                .lectureId(1L)
+                .lectureItemId(1L)
+                .enrolledAt(LocalDateTime.parse("2024-10-01T10:00"))
+                .build();
+        LectureEnrollment enrollment2 = LectureEnrollment.builder()
+                .lectureId(2L)
+                .lectureItemId(2L)
+                .build();
+
+        // Mocking repository responses
+        given(lectureEnrollmentRepository.findAllByUserId(userId)).willReturn(List.of(enrollment1, enrollment2));
+        given(lectureRepository.getById(1L)).willReturn(lecture1);
+        given(lectureRepository.getById(2L)).willReturn(lecture2);
+        given(lectureRepository.getItemById(1L, 1L)).willReturn(lectureItem1);
+        given(lectureRepository.getItemById(2L, 2L)).willReturn(lectureItem2);
+
+        // When
+        List<LectureEnrollmentInfo> userLectureEnrollments = lectureService.getUserLectureEnrollments(userId);
+
+        // Then
+        assertAll(
+                () -> assertEquals(2, userLectureEnrollments.size()),
+                () -> {
+                    // 첫 번째 강의 검증
+                    final LectureEnrollmentInfo firstLectureEnrollment = userLectureEnrollments.get(0);
+                    assertEquals("특강1", firstLectureEnrollment.getLecture().getTitle());
+                    assertEquals(1L, firstLectureEnrollment.getLecture().getId());
+                    assertEquals(LocalDateTime.parse("2024-10-01T10:00"), firstLectureEnrollment.getLectureItem().getLectureTime());
+
+                    // 두 번째 강의 검증
+                    final LectureEnrollmentInfo secondLectureEnrollment = userLectureEnrollments.get(1);
+                    assertEquals("특강2", secondLectureEnrollment.getLecture().getTitle());
+                    assertEquals(2L, secondLectureEnrollment.getLecture().getId());
+                    assertEquals(LocalDateTime.parse("2024-10-01T10:00"), secondLectureEnrollment.getLectureItem().getLectureTime());
+                }
+        );
     }
 }
--- src/test/java/io/hhplus/cleanarchitecture/lecture/infrastruture/persistence/repository/LectureRepositoryImplTest.java
+++ src/test/java/io/hhplus/cleanarchitecture/lecture/infrastruture/persistence/repository/LectureRepositoryImplTest.java
@@ -1,25 +1,23 @@
 package io.hhplus.cleanarchitecture.lecture.infrastruture.persistence.repository;
 
-import io.hhplus.cleanarchitecture.common.time.SystemTimeProvider;
-import io.hhplus.cleanarchitecture.common.time.TimeProvider;
 import io.hhplus.cleanarchitecture.lecture.domain.entity.Lecture;
+import io.hhplus.cleanarchitecture.lecture.domain.entity.LectureItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
 @DataJpaTest
-@ContextConfiguration(classes = LectureRepositoryImplTest.TestConfig.class)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @TestPropertySource("classpath:/application-test.yml")
 class LectureRepositoryImplTest {
@@ -28,13 +26,13 @@ class LectureRepositoryImplTest {
     private LectureJpaRepository lectureJpaRepository;
 
     @Autowired
-    private TimeProvider timeProvider;
+    private LectureItemJpaRepository lectureItemJpaRepository;
 
     private LectureRepositoryImpl lectureRepository;
 
     @BeforeEach
     public void setUp() {
-        lectureRepository = new LectureRepositoryImpl(lectureJpaRepository, timeProvider);
+        lectureRepository = new LectureRepositoryImpl(lectureJpaRepository, lectureItemJpaRepository);
     }
 
     @Test
@@ -42,10 +40,7 @@ class LectureRepositoryImplTest {
         // Given
         Lecture lecture = Lecture.builder()
                 .title("클린 아키텍처")
-                .speaker("로버트 C. 마틴")
-                .maxEnrollmentCount(30)
-                .enrolledCount(0)
-                .enrollOpenAt(LocalDateTime.parse("2024-10-01T10:00"))
+                .instructor("로버트 C. 마틴")
                 .build();
 
         // When
@@ -56,45 +51,63 @@ class LectureRepositoryImplTest {
         assertAll(
                 () -> then(savedLecture.getId()).isNotNull(),
                 () -> then(savedLecture.getTitle()).isEqualTo("클린 아키텍처"),
-                () -> then(savedLecture.getSpeaker()).isEqualTo("로버트 C. 마틴"),
-                () -> then(savedLecture.getMaxEnrollmentCount()).isEqualTo(30),
-                () -> then(savedLecture.getEnrolledCount()).isEqualTo(0),
-                () -> then(savedLecture.getEnrollOpenAt()).isEqualTo(LocalDateTime.parse("2024-10-01T10:00"))
+                () -> then(savedLecture.getInstructor()).isEqualTo("로버트 C. 마틴")
         );
     }
 
     @Test
-    void 강의_ID로_강의를_조회할_수_있다() {
+    void 강의_아이템을_저장할_수_있다() {
         // Given
-        Lecture lecture = Lecture.builder()
-                .title("클린 아키텍처")
-                .speaker("로버트 C. 마틴")
-                .maxEnrollmentCount(30)
-                .enrolledCount(0)
-                .enrollOpenAt(LocalDateTime.parse("2024-10-01T10:00"))
+        final LectureItem lectureItem = LectureItem.builder()
+                .lectureId(1L)
+                .capacity(30)
+                .remainingCapacity(30)
+                .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
                 .build();
-        Lecture savedLecture = lectureRepository.save(lecture);
 
         // When
-        Lecture foundLecture = lectureRepository.getById(savedLecture.getId());
+        final LectureItem savedLectureItem = lectureRepository.save(lectureItem);
 
         // Then
         // 한 번에 검사
         assertAll(
-                () -> then(foundLecture.getId()).isEqualTo(savedLecture.getId()),
-                () -> then(foundLecture.getTitle()).isEqualTo("클린 아키텍처"),
-                () -> then(foundLecture.getSpeaker()).isEqualTo("로버트 C. 마틴"),
-                () -> then(foundLecture.getMaxEnrollmentCount()).isEqualTo(30),
-                () -> then(foundLecture.getEnrolledCount()).isEqualTo(0),
-                () -> then(foundLecture.getEnrollOpenAt()).isEqualTo(LocalDateTime.parse("2024-10-01T10:00"))
+                () -> then(savedLectureItem.getId()).isNotNull(),
+                () -> then(savedLectureItem.getLectureId()).isEqualTo(1L),
+                () -> then(savedLectureItem.getCapacity()).isEqualTo(30),
+                () -> then(savedLectureItem.getRemainingCapacity()).isEqualTo(30),
+                () -> then(savedLectureItem.getLectureTime()).isEqualTo(LocalDateTime.parse("2024-10-01T10:00"))
         );
     }
 
-    @TestConfiguration
-    static class TestConfig {
-        @Bean
-        public TimeProvider timeProvider() {
-            return new SystemTimeProvider();
-        }
+    @Test
+    void 강의_아이템을_여러개_저장할_수_있다() {
+        // Given
+        final LectureItem lectureItem1 = LectureItem.builder()
+                .lectureId(1L)
+                .capacity(30)
+                .remainingCapacity(30)
+                .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
+                .build();
+
+        final LectureItem lectureItem2 = LectureItem.builder()
+                .lectureId(1L)
+                .capacity(30)
+                .remainingCapacity(30)
+                .lectureTime(LocalDateTime.parse("2024-10-01T10:00"))
+                .build();
+
+        // When
+        final List<LectureItem> lectureItems = lectureRepository.saveAll(List.of(lectureItem1, lectureItem2));
+
+        // Then
+        // 한 번에 검사
+        assertAll(
+                () -> then(lectureItems.size()).isEqualTo(2),
+                () -> assertThat(lectureItems).extracting("lectureId", "capacity", "remainingCapacity", "lectureTime")
+                        .containsExactlyInAnyOrder(
+                                tuple(1L, 30, 30, LocalDateTime.parse("2024-10-01T10:00")),
+                                tuple(1L, 30, 30, LocalDateTime.parse("2024-10-01T10:00"))
+                        )
+        );
     }
 }
