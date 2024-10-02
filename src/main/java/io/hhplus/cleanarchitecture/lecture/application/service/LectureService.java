09eb468c36e8a5a574cdd8c88951d4585522827b
rueun <dkfah1229@naver.com>
2024-10-02 12:43:27 +0900

    feat: LectureService 구현 - 사용자가 신청한 강의 목록, 사용자가 신청할 수 있는 강의 목록 조회

--- src/main/java/io/hhplus/cleanarchitecture/lecture/application/service/LectureService.java
+++ src/main/java/io/hhplus/cleanarchitecture/lecture/application/service/LectureService.java
@@ -2,46 +2,102 @@ package io.hhplus.cleanarchitecture.lecture.application.service;
 
 import io.hhplus.cleanarchitecture.common.time.TimeProvider;
 import io.hhplus.cleanarchitecture.lecture.application.dto.EnrollLectureCommand;
+import io.hhplus.cleanarchitecture.lecture.application.dto.LectureWithItems;
 import io.hhplus.cleanarchitecture.lecture.domain.entity.Lecture;
 import io.hhplus.cleanarchitecture.lecture.domain.entity.LectureEnrollment;
+import io.hhplus.cleanarchitecture.lecture.domain.entity.LectureItem;
 import io.hhplus.cleanarchitecture.lecture.domain.repository.LectureEnrollmentRepository;
 import io.hhplus.cleanarchitecture.lecture.domain.repository.LectureRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 @Service
 public class LectureService {
     private final LectureRepository lectureRepository;
     private final LectureEnrollmentRepository lectureEnrollmentRepository;
+
     private final TimeProvider timeProvider;
 
+    /**
+     * 특강 신청
+     * @param command 특강 신청 Command 객체
+     * @return 신청된 LectureEnrollment 객체
+     * @throws IllegalArgumentException 유저가 동일한 강의에 이미 수강신청한 경우
+     */
     @Transactional
-    public void enroll(final EnrollLectureCommand command) {
-        final LocalDateTime now = timeProvider.now();
+    public LectureEnrollment enroll(final EnrollLectureCommand command) {
+        LocalDateTime enrolledAt = timeProvider.now();
 
-        final Lecture lecture = lectureRepository.getById(command.getLectureId());
-        lecture.enrollStudent(now);
+        // TODO: 2024-10-02 동시성 제어를 위해 Pessimistic Locking을 사용해야 함
+        final LectureItem lectureItem = lectureRepository.getItemById(command.getLectureId(), command.getLectureItemId());
+        lectureItem.enroll(enrolledAt);
 
-        final LectureEnrollment enrollment = LectureEnrollment.of(command.getLectureId(), command.getUserId(), now);
+        final LectureEnrollment enrollment = LectureEnrollment.of(command.getLectureId(), command.getLectureItemId(), command.getUserId(), enrolledAt);
         if (lectureEnrollmentRepository.existsByLectureIdAndUserId(command.getLectureId(), command.getUserId())) {
             throw new IllegalArgumentException("해당 유저는 이미 수강신청을 했습니다.");
         }
 
-        lectureRepository.save(lecture);
-        lectureEnrollmentRepository.save(enrollment);
+        lectureRepository.save(lectureItem);
+        return lectureEnrollmentRepository.save(enrollment);
     }
 
+
+    /**
+     * 특정 유저가 특정한 특강에 신청했는지 확인한다.
+     * @param lectureId 특강 ID
+     * @param userId 유저 ID
+     * @return 특강 신청 여부
+     */
     public boolean checkEnrollment(final Long lectureId, final Long userId) {
         return lectureEnrollmentRepository.existsByLectureIdAndUserId(lectureId, userId);
     }
 
-    public Page<Lecture> getAvailableLectures(final Long userId, final Pageable pageable) {
-        return lectureRepository.findAvailableLectures(userId, pageable);
+    /**
+     * 사용자가 신청 가능한 강의 목록을 가져온다.
+     * 수강 가능한 강의는 아직 수강 신청이 안된 강의 중에서, 잔여 수량이 있는 강의만을 의미한다.
+     * @param userId 사용자 ID
+     * @return 사용자가 수강 가능한 강의 목록
+     */
+    public List<LectureWithItems> getAvailableLectures(final Long userId) {
+        final List<Lecture> availableLectures = lectureRepository.getAvailableLectures(userId);
+        final Map<Long, List<LectureItem>> lectureItemMap = lectureRepository.getLectureItemMap(
+                availableLectures.stream().map(Lecture::getId)
+                        .toList());
+
+        return availableLectures.stream()
+                .map(lecture -> {
+                    // 해당 강의의 아이템 리스트에서 remainingCapacity > 0인 것만 필터링
+                    final List<LectureItem> availableItems = lectureItemMap.getOrDefault(lecture.getId(), List.of())
+                            .stream()
+                            .filter(item -> item.getRemainingCapacity() > 0)
+                            .toList();
+                    return LectureWithItems.of(lecture, availableItems);
+                })
+                .filter(lectureWithItems -> !lectureWithItems.getItems().isEmpty()) // 아이템이 있는 경우에만 강의를 포함
+                .toList();
     }
+
+    /**
+     * 사용자가 신청한 모든 강의 목록을 가져온다.
+     * @param userId 사용자 ID
+     * @return 사용자가 신청한 모든 강의 목록
+     */
+    public List<LectureWithItems> getUserLectures(final Long userId) {
+        // 사용자의 모든 강의 신청 기록을 가져옴
+        final List<LectureEnrollment> enrollments = lectureEnrollmentRepository.findAllByUserId(userId);
+
+        return enrollments.stream()
+                .map(enrollment -> {
+                    final Lecture lecture = lectureRepository.getById(enrollment.getLectureId());
+                    final LectureItem item = lectureRepository.getItemById(enrollment.getLectureId(), enrollment.getLectureItemId());
+                    return LectureWithItems.of(lecture, List.of(item));
+                }).toList();
+    }
+
 }
